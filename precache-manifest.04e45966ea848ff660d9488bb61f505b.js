7f7c14c2ef802dc75448ee9a5868cbec8efad2d5
kertesz karoly <kerteszkaroly@kerteszs-Mac-mini.local>
2020-10-21 00:42:08 +0200

    Updates

---
+++ precache-manifest.04e45966ea848ff660d9488bb61f505b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9e458f88357852bec7960a1c5662bd7",
+    "url": "/Monster-App/index.html"
+  },
+  {
+    "revision": "654c210657715a317787",
+    "url": "/Monster-App/static/css/main.abcefa28.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/Monster-App/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Monster-App/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "654c210657715a317787",
+    "url": "/Monster-App/static/js/main.267973da.chunk.js"
+  },
+  {
+    "revision": "80d8bb76d33cf0a06280",
+    "url": "/Monster-App/static/js/runtime-main.e7c11096.js"
+  }
+]);
